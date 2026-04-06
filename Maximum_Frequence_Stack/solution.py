0d4a4b197f1d65aceb7f5587a3a7926b89205bed
zahariukpn-cs <zahariuk.pn@ucu.edu.ua>
2026-04-06 22:30:37 +0300

    Final dir bur with empty file.

---
+++ Maximum_Frequence_Stack/solution.py
@@ -0,0 +1,16 @@
+class FreqStack:
+
+    def __init__(self):
+        pass
+
+    def push(self, val: int) -> None:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
